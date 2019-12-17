df693668fcb4ce622845441f41e0bde8c91bd43c
Ola Siem <info@siemieniuk.net>
2019-12-17 14:11:51 +0100

    Add loop for generate gender

--- gender.js
+++ gender.js
@@ -5,19 +5,34 @@ const maleNames = ['Kurt', 'Michael', 'George', 'Jarosław'];
 const femaleNames = ['Christina', 'Krystyna', 'Diana', 'Ola'];
 const lastNames = ['Aguillera', 'Cobain', 'Jackson', 'Michael', 'Czubówna', 'Kaczyński'];
 
-const arr = [genders, maleNames, femaleNames, lastNames];
-
-const randChoice = () => {  
+const randChoice = arr => {  
+  console.log(arr);
   return arr[Math.floor(arr.length * Math.random())];
 };
 
 const people = [];
 
-// for (let i = 0; i < 20; i++) { } 
+for (let i = 0; i < 20; i++) { 
+  const gender = randChoice(genders);
+  let age = Math.floor((Math.random() * 100) + 1);
 
-const jsonString = JSON.stringify(people);
+  if(gender === "male"){
+    maleName = randChoice(maleNames);
+    lastName = randChoice(lastNames);
+    people.push({ name:maleName, surname:lastName, gender, age });
+  } 
+    else {
+    femaleName = randChoice(femaleNames);
+    lastName = randChoice(lastNames);
+    people.push({ name:femaleName, surmane:lastName, gender, age });
+  }
+} 
 
-fs.writeFile('people.json', jsonString, err => {
+console.log(people);
+
+const jsonData = JSON.stringify(people);
+
+fs.writeFile('people.json', jsonData, err => {
   if (err) throw err;
-  console.log('The file has been saved!');
+  console.log('File has been successfully generated! Check people.json');
 });
